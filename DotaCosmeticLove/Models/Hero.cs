25502026d26e25938fe0b3d4a07fbecb4e8bc1d5
Shaun Smith <45996226+1vertical@users.noreply.github.com>
2021-03-08 23:12:48 +0200

    InitialCommit

---
+++ DotaCosmeticLove/Models/Hero.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotaCosmeticLove.Models
+{
+    public class Hero
+    {
+        public int HeroId { get; set; }
+        public string ImageUrl { get; set; }
+        public string PrimaryAttribute { get; set; }
+        public bool IsWorkshopReady { get; set; }
+        public int ReleaseYear { get; set; }
+        public int ItemSlots { get; set; }
+        public int ItemBundles { get; set; }
+        public int ItemTotal { get; set; } //LoveRating
+        public float ItemToBundleRatio { get; set; }
+        public int TotalWeapons { get; set; }
+        public int TotalArcanas { get; set; }
+        public int TotalPersonas { get; set; }
+        public int TotalLegs { get; set; }
+        public int TotalArms { get; set; }
+        public int TotalLeftArms { get; set; }
+        public int TotalArmors { get; set; }
+        public int TotalTaunts { get; set; }
+        public int TotalHeads { get; set; }
+        public int TotalNecks { get; set; }
+        public int TotalBacks { get; set; }
+        public int TotalTails { get; set; }
+        public int TotalMisc { get; set; }
+        public int TotalShoulders { get; set; }
+        public int TotalBelts { get; set; }
+        public int TotalBoars { get; set; }
+        public int TotalHawks { get; set; }
+        public int TotalBarrels { get; set; }
+        public int TotalKineticGems { get; set; }
+        public int TotalShields { get; set; }
+        public int TotalRocketFlares { get; set; }
+        public int TotalPowerCogs { get; set; }
+        public int TotalWings { get; set; }
+        public int TotalElderDragonForms { get; set; }
+        public int TotalStoneRemnants { get; set; }
+        public int TotalTotems { get; set; }
+        public int TotalOffhands { get; set; }
+        public int TotalGloves { get; set; }
+        public int TotalBanners { get; set; }
+        public int TotalShapeshifts { get; set; }
+        public int TotalWolfs { get; set; }
+        public int TotalSupernovas { get; set; }
+        public int TotalTiny { get; set; }
+        public int TotalSmall { get; set; }
+        public int TotalMedium { get; set; }
+        public int TotalLarge { get; set; }
+        public int TotalFists { get; set; }
+        public int TotalTusks { get; set; }
+        public int TotalFrozenSigil { get; set; }
+        public int TotalTombstones { get; set; }
+        public int TotalFleshGolem { get; set; }
+        public int Total { get; set; }
+        public int Total { get; set; }
+        public int Total { get; set; }
+        public int Total { get; set; }
+        public int Total { get; set; }
+        public int Total { get; set; }
+        public int Total { get; set; }
+        public int Total { get; set; }
+        public int Total { get; set; }
+        public int Total { get; set; }
+        public int Total { get; set; }
+        public int Total { get; set; }
+        public int Total { get; set; }
+        public int Total { get; set; }
+        public int Total { get; set; }
+        public int Total { get; set; }
+        public int TotalMounts { get; set; }
+        public int ChangeSinceLastUpdate { get; set; }
+        public PopRating Popularity { get; set; }
+    }
+}
---
+++ DotaCosmeticLove/Models/PopRating.cs
@@ -0,0 +1,8 @@
+﻿namespace DotaCosmeticLove.Models
+{
+    public class PopRating
+    {
+        public int PopRatingId { get; set; }
+        public string Rating { get; set; }
+    }
+}
